4175041f5158abaace60887fb571f1861988a62d
Olávio Timóteo <olaviotimoteo@hotmail.com>
2022-06-04 22:15:46 -0300

    implements function createClient

--- src/Utils/requestAPI.js
+++ src/Utils/requestAPI.js
@@ -4,6 +4,11 @@ const api = axios.create({
   baseURL: 'http://localhost:3003',
 });
 
+export const createClient = async (endpoint, body) => {
+  const { data } = await api.create(endpoint, body);
+  return data;
+}
+
 export const getAllClients = async (endpoint) => {
   const { data } = await api.get(endpoint);
   return data;
