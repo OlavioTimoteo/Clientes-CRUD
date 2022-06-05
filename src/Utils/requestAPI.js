118b11e2abbcdb73b904e6e4a9de640ab18bda47
Olávio Timóteo <olaviotimoteo@hotmail.com>
2022-06-04 22:14:08 -0300

    implements function getClientsById

--- src/Utils/requestAPI.js
+++ src/Utils/requestAPI.js
@@ -4,7 +4,13 @@ const api = axios.create({
   baseURL: 'http://localhost:3003',
 });
 
-export const getAllClientes = async (endpoint) => {
+export const getAllClients = async (endpoint) => {
+  const { data } = await api.get(endpoint);
+  return data;
+}
+
+
+export const getClientsById = async (endpoint) => {
   const { data } = await api.get(endpoint);
   return data;
 }
